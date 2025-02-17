6996a3a29aca6e398bef8843aeef0cf729d40be7
francisco-JGC <jjjchico1@gmail.com>
2025-02-16 22:43:06 -0600

    feat: update 404 page buttons to use Button component

--- src/pages/error404/404.tsx
+++ src/pages/error404/404.tsx
@@ -1,7 +1,6 @@
 import { Button } from "../../shared/components/Button"
 import 'boxicons/css/boxicons.min.css';
 
-
 export const Page404 = () => {
   return (
     <section>
@@ -13,16 +12,25 @@ export const Page404 = () => {
             <p className="mt-4 text-gray-500">We searched high and low, but couldn’t find what you’re looking for.Let’s find a better place for you to go.</p>
 
             <div className="flex items-center w-full mt-6 gap-x-3 shrink-0 sm:w-auto">
-              <Button className="bg-red-100">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5 rtl:rotate-180">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
-                </svg>
+              <Button
+                variant="secondary"
+                useAnimation
+                icon={
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5 rtl:rotate-180">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
+                  </svg>
+                }
+              >
+
                 <span>Go back</span>
               </Button>
 
-              <button className="w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600">
+              <Button
+                variant="primary"
+                useAnimation
+              >
                 Take me home
-              </button>
+              </Button>
             </div>
           </div>
         </div>
