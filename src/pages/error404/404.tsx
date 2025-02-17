ecbcefc5de614a288e46f23dbb43cb31713e209b
francisco-JGC <jjjchico1@gmail.com>
2025-02-17 13:57:09 -0600

    fix: update Button component import and remove unused props in 404 page

--- src/pages/error404/404.tsx
+++ src/pages/error404/404.tsx
@@ -1,6 +1,6 @@
-import { Button } from "../../shared/components/Button"
 import 'boxicons/css/boxicons.min.css';
 import { useTranslation } from "react-i18next";
+import { Button } from '@/components/ui/button';
 
 export const Page404 = () => {
   const { t } = useTranslation()
@@ -17,20 +17,14 @@ export const Page404 = () => {
             <div className="flex items-center w-full mt-6 gap-x-3 shrink-0 sm:w-auto">
               <Button
                 variant="secondary"
-                useAnimation
-                icon={
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5 rtl:rotate-180">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
-                  </svg>
-                }
               >
-
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5 rtl:rotate-180">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
+                </svg>
                 <span>{t('page_404.buttons.back')}</span>
               </Button>
 
               <Button
-                variant="primary"
-                useAnimation
               >
                 <span>{t('page_404.buttons.take_me_home')}</span>
               </Button>
