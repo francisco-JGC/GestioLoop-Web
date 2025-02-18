17a16f111d40ef56191bf5268d04301743e9591b
francisco-JGC <jjjchico1@gmail.com>
2025-02-18 10:33:30 -0600

    refactor: make responsive design fot signin and register component

--- src/pages/login/register/RegisterPage.tsx
+++ src/pages/login/register/RegisterPage.tsx
@@ -11,15 +11,13 @@ export const RegisterPage = () => {
     <div>
       <HeaderLogin show_button="login" />
 
-      <div className="flex w-full h-screen justify-center items-center max-w-[1300px]">
-        <div className="flex items-center justify-center gap-10 w-full">
-          <div className="w-full flex flex-col gap-10 max-w-[400px]">
+      <div className="flex w-full min-h-screen justify-center items-center max-w-[1300px] p-4">
+        <div className="flex flex-col md:flex-row items-center justify-center gap-10 w-full">
+          <div className="w-full flex flex-col gap-6 max-w-[400px]">
             <div>
-              <span className="text-lg">{t('login.title')}</span>
+              <span className="text-2xl font-semibold">{t('login.title')}</span>
             </div>
-            <form className="flex flex-col gap-6"
-              autoComplete="off"
-            >
+            <form className="flex flex-col gap-6" autoComplete="off">
               <Input
                 placeholder={t('login.placeholder-username')}
                 type="text"
@@ -52,16 +50,16 @@ export const RegisterPage = () => {
                 autoComplete="off"
                 name="phone_number"
               />
-              <div className="flex justify-between items-center">
+              <div className="flex flex-col md:flex-row justify-between items-center gap-4">
                 <span className="text-lg text-gray-400">{t('login.comment')}</span>
-                <Button className="rounded-full p-6">
+                <Button className="rounded-full p-5 w-full md:w-auto">
                   {t('login.register')}
                 </Button>
               </div>
             </form>
           </div>
-          <div className="w-full max-w-[400px]">
-            <Image src="" width={400} height={400} className="w-[400px] h-[400px]" />
+          <div className="w-full max-w-[400px] hidden md:block">
+            <Image src="" width={400} height={400} className="w-full h-auto" />
           </div>
         </div>
       </div>
--- src/pages/login/signIn/SignInPage.tsx
+++ src/pages/login/signIn/SignInPage.tsx
@@ -12,15 +12,13 @@ export const SigninPage = () => {
     <div>
       <HeaderLogin show_button="register" />
 
-      <div className="flex w-full h-screen justify-center items-center max-w-[1300px]">
-        <div className="flex items-center justify-center gap-10 w-full">
-          <div className="w-full flex flex-col gap-10 max-w-[400px]">
+      <div className="flex w-full min-h-screen justify-center items-center max-w-[1300px] p-4">
+        <div className="flex flex-col md:flex-row items-center justify-center gap-10 w-full">
+          <div className="w-full flex flex-col gap-6 max-w-[400px]">
             <div>
-              <span className="text-lg">{t('login.title')}</span>
+              <span className="text-2xl font-semibold">{t('login.title')}</span>
             </div>
-            <form className="flex flex-col gap-6"
-              autoComplete="off"
-            >
+            <form className="flex flex-col gap-6" autoComplete="off">
               <Input
                 placeholder={t('login.placeholder-email')}
                 type="email"
@@ -37,16 +35,18 @@ export const SigninPage = () => {
                 autoComplete="off"
                 name="password"
               />
-              <div className="flex justify-between items-center">
-                <Link to={"#"} className="text-lg">{t('login.forgot-password')}</Link>
-                <Button className="rounded-full p-6">
+              <div className="flex flex-col md:flex-row justify-between items-center gap-4">
+                <Link to={"#"} className="text-lg">
+                  {t('login.forgot-password')}
+                </Link>
+                <Button className="rounded-full p-5 w-full md:w-auto">
                   {t('login.signin')}
                 </Button>
               </div>
             </form>
           </div>
-          <div className="w-full max-w-[400px]">
-            <Image src="" width={400} height={400} className="w-[400px] h-[400px]" />
+          <div className="w-full max-w-[400px] hidden md:block">
+            <Image src="" width={400} height={400} className="w-full h-auto" />
           </div>
         </div>
       </div>
