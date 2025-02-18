70be9361f4acbfedb6889a82d98fb7eef4539411
francisco-JGC <jjjchico1@gmail.com>
2025-02-18 15:48:59 -0600

    feat: add Zustand store for authentication management with login, logout, token refresh, and session check

---
+++ src/shared/store/authStore.ts
@@ -0,0 +1,104 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { create } from 'zustand'
+import {
+  login,
+  refreshToken,
+  logout,
+  checkSession,
+} from '@/infrastructure/api/hooks/authHook'
+import { HttpStatusCode } from 'axios'
+
+interface User {
+  id: string
+  email: string
+  username: string
+  user_role: string
+  user_type: string
+  tenantId: string | null
+}
+
+interface AuthState {
+  user: User | null
+  isAuthenticated: boolean
+  isLoading: boolean
+  error: string | null
+  login: (session_field: string, password: string) => Promise<void>
+  refreshToken: () => Promise<void>
+  logout: () => Promise<void>
+  checkSession: () => Promise<void>
+}
+
+const useAuthStore = create<AuthState>((set) => ({
+  user: null,
+  isAuthenticated: false,
+  isLoading: false,
+  error: null,
+
+  login: async (session_field, password) => {
+    set({ isLoading: true, error: null })
+
+    try {
+      const response = await login(session_field, password)
+
+      if (response.statusCode === HttpStatusCode.Ok) {
+        set({ isAuthenticated: true, user: response.data, error: null })
+      } else {
+        set({
+          error: 'Login failed',
+        })
+      }
+    } catch (e) {
+      set({
+        error: 'Login failed',
+      })
+    } finally {
+      set({ isLoading: false })
+    }
+  },
+
+  refreshToken: async () => {
+    set({ error: null })
+
+    try {
+      const response = await refreshToken()
+
+      if (response.statusCode !== HttpStatusCode.Ok) {
+        set({ isAuthenticated: false, user: null })
+      }
+    } catch (e) {
+      set({ error: 'Failed to refresh token' })
+    }
+  },
+
+  logout: async () => {
+    set({ error: null })
+
+    try {
+      await logout()
+      set({ user: null, isAuthenticated: false, error: null })
+    } catch (error) {
+      set({ error: 'bad request' })
+      throw error
+    }
+  },
+
+  checkSession: async () => {
+    set({ error: null })
+
+    try {
+      const response = await checkSession()
+
+      if (response.statusCode !== HttpStatusCode.Ok) {
+        set({ isAuthenticated: false, user: null })
+        throw new Error('Session check failed')
+      } else {
+        set({ user: response?.data?.user, isAuthenticated: true, error: null })
+      }
+    } catch (e) {
+      set({ user: null, isAuthenticated: false, error: 'Session check failed' })
+      throw e
+    }
+  },
+}))
+
+export default useAuthStore
