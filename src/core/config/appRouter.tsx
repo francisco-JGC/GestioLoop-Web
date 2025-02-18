1d6a6c69a56ff557a29245d16e4ce893a42fe3d3
francisco-JGC <jjjchico1@gmail.com>
2025-02-18 01:18:49 -0600

    feat: add sign-in and register routes, enhance 404 page with navigation options

--- src/core/config/appRouter.tsx
+++ src/core/config/appRouter.tsx
@@ -1,11 +1,21 @@
 import { createBrowserRouter } from 'react-router-dom'
 import { HomePage } from '../../pages/home/HomePage'
 import { Page404 } from '../../pages/error404/404'
+import { SigninPage } from '@/pages/login/signIn/SignInPage'
+import { RegisterPage } from '@/pages/login/register/RegisterPage'
 
 const AppRouter = createBrowserRouter([
   {
     path: '/',
     element: <HomePage />,
+  },
+  {
+    path: 'sign-in',
+    element: <SigninPage />
+  },
+  {
+    path: 'register',
+    element: <RegisterPage />
   },
   {
     path: '*',
--- src/pages/error404/404.tsx
+++ src/pages/error404/404.tsx
@@ -1,9 +1,11 @@
 import 'boxicons/css/boxicons.min.css';
 import { useTranslation } from "react-i18next";
 import { Button } from '@/components/ui/button';
+import { Link, useNavigate } from 'react-router-dom';
 
 export const Page404 = () => {
   const { t } = useTranslation()
+  const navigate = useNavigate()
 
   return (
     <section>
@@ -12,11 +14,13 @@ export const Page404 = () => {
           <div className="flex flex-col items-center max-w-lg mx-auto text-center">
             <p className="text-sm font-medium text-blue-500">{t('page_404.title')}</p>
             <h1 className="mt-3 text-2xl font-semibold text-gray-800">{t('page_404.subject')}</h1>
-            <p className="mt-4 text-gray-500">{t('page_404.description')}</p>
+            <p className="mt-4 text-gray-500 text-lg">{t('page_404.description')}</p>
 
             <div className="flex items-center w-full mt-6 gap-x-3 shrink-0 sm:w-auto">
+
               <Button
                 variant="secondary"
+                onClick={() => navigate(-1)}
               >
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5 rtl:rotate-180">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
@@ -24,14 +28,16 @@ export const Page404 = () => {
                 <span>{t('page_404.buttons.back')}</span>
               </Button>
 
-              <Button
-              >
-                <span>{t('page_404.buttons.take_me_home')}</span>
-              </Button>
+              <Link to={'/'}>
+                <Button
+                >
+                  <span>{t('page_404.buttons.take_me_home')}</span>
+                </Button>
+              </Link>
             </div>
           </div>
         </div>
       </div>
-    </section>
+    </section >
   )
 }
