d92d15a1a0ccfaea86b9c5913035d88e8e33d320
francisco-JGC <jjjchico1@gmail.com>
2025-02-23 14:20:38 -0600

    feat: add breadcrumb navigation to Dashboard layout

--- src/pages/dashboard/dashboardPage.tsx
+++ src/pages/dashboard/dashboardPage.tsx
@@ -1,8 +1,13 @@
 import { DashboardLayout } from "../layouts/dashboardLayout"
 
+const breadcrumb = [{
+  label: 'Dashboard',
+  path: '/dashboard'
+}]
+
 export const DashboardPage = () => {
   return (
-    <DashboardLayout>
+    <DashboardLayout breadcrumbList={breadcrumb}>
       <div>
         <h1>Dashboard page</h1>
       </div>
--- src/pages/layouts/dashboardLayout.tsx
+++ src/pages/layouts/dashboardLayout.tsx
@@ -1,13 +1,46 @@
 import { AppSidebar } from "@/components/app-sidebar"
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbList,
+  BreadcrumbSeparator
+} from "@/components/ui/breadcrumb"
+import { Link } from "react-router-dom"
 
-export const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+interface IProps {
+  children: React.ReactNode
+  breadcrumbList: {
+    path: string,
+    label: string
+  }[]
+}
+
+export const DashboardLayout = ({ children, breadcrumbList }: IProps) => {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <main>
-        <SidebarTrigger />
-        {children}
+      <main className="w-full flex flex-col gap-4 ">
+        <div className="flex items-center">
+          <SidebarTrigger />
+          <Breadcrumb>
+            <BreadcrumbList>
+              {
+                breadcrumbList?.map((item, index) => (
+                  <>
+                    <BreadcrumbSeparator />
+                    <BreadcrumbItem key={index} className="hover:text-black">
+                      <Link to={item.path} >{item.label}</Link>
+                    </BreadcrumbItem>
+                  </>
+                ))
+              }
+            </BreadcrumbList>
+          </Breadcrumb>
+        </div>
+        <div>
+          {children}
+        </div>
       </main>
     </SidebarProvider>
   )
