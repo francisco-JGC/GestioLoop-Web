fd0e06c843cfba1041b1e7cb2334ed14b645a21e
francisco-JGC <jjjchico1@gmail.com>
2025-02-24 01:35:44 -0600

    feat: add NavBranch component for branch selection in sidebar

---
+++ src/components/nav-branch.tsx
@@ -0,0 +1,85 @@
+import {
+  ChevronsUpDown,
+} from "lucide-react"
+
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import {
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from "@/components/ui/sidebar"
+import useBranchesStore from "@/shared/store/branchesStore"
+import { useEffect } from "react"
+import { getBranches } from "@/infrastructure/api/hooks/branchesHook"
+
+export function NavBranch() {
+  const { isMobile } = useSidebar()
+  const { branches, selectedBranch, setBranches, setSelectedBranch } = useBranchesStore()
+
+  useEffect(() => {
+    if (!branches) {
+      getBranches()
+        .then((response) => {
+          setBranches(response ?? [])
+          setSelectedBranch(response ? response[0].id : '')
+        })
+    }
+  }, [branches, setBranches, setSelectedBranch])
+
+  return (
+    <SidebarMenu>
+      <SidebarMenuItem>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <SidebarMenuButton
+              size="lg"
+              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground bg-gray-100"
+            >
+              <Avatar className="h-8 w-8 rounded-lg">
+                <AvatarImage src={''} alt="dqwd" />
+                <AvatarFallback className="rounded-lg uppercase"><i className='bx bxs-briefcase-alt-2' ></i></AvatarFallback>
+              </Avatar>
+              <div className="grid flex-1 text-left text-sm leading-tight max-w-60" title={selectedBranch?.name}>
+                <span className="truncate font-semibold">{selectedBranch?.name}</span>
+                <span className="truncate text-xs">{selectedBranch?.address}</span>
+              </div>
+              <ChevronsUpDown className="ml-auto size-4" />
+            </SidebarMenuButton>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            className="min-w-56 rounded-lg"
+            side={isMobile ? "bottom" : "right"}
+            align="end"
+            sideOffset={4}
+          >
+            {
+              branches?.map((branch) => (
+                <div key={branch.id} className={`${branch.id === selectedBranch?.id && 'font-semibold'}`}
+                  onClick={() => setSelectedBranch(branch.id)}
+                >
+                  <DropdownMenuItem className="">
+                    <i className={`bx ${branch.id === selectedBranch?.id ? 'bxs' : 'bx'}-briefcase-alt-2`}></i>
+                    {branch.name}
+                  </DropdownMenuItem>
+                  <DropdownMenuSeparator />
+                </div>
+              ))
+            }
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </SidebarMenuItem>
+    </SidebarMenu>
+  )
+}
