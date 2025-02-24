ac5ee82589c873aec46bde4c0c1f1d2a7f8d7542
francisco-JGC <jjjchico1@gmail.com>
2025-02-23 20:07:37 -0600

    feat: remove static breadcrumb from DashboardPage component

--- src/pages/dashboard/dashboardPage.tsx
+++ src/pages/dashboard/dashboardPage.tsx
@@ -1,13 +1,8 @@
 import { DashboardLayout } from "../layouts/dashboardLayout"
 
-const breadcrumb = [{
-  label: 'Dashboard',
-  path: '/dashboard'
-}]
-
 export const DashboardPage = () => {
   return (
-    <DashboardLayout breadcrumbList={breadcrumb}>
+    <DashboardLayout>
       <div>
         <h1>Dashboard page</h1>
       </div>
