97adbc643daa828458360a29a738d312a87df9a5
francisco-JGC <jjjchico1@gmail.com>
2025-02-25 17:14:15 -0600

    feat: add BranchesHeader component to BranchesPage for improved layout

--- src/pages/dashboard/branches/branchesPage.tsx
+++ src/pages/dashboard/branches/branchesPage.tsx
@@ -1,11 +1,12 @@
 import { DashboardLayout } from "../../layouts/dashboardLayout"
+import { BranchesHeader } from "./components/branch-header"
 import { BranchesLists } from "./components/branches-lists"
 
 export const BranchesPage = () => {
   return (
     <DashboardLayout>
       <div>
-
+        <BranchesHeader />
         <BranchesLists />
       </div>
     </DashboardLayout>
