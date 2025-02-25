6ae731c2b9ec03282b046fb395ed91ce3542e76a
francisco-JGC <jjjchico1@gmail.com>
2025-02-24 19:26:46 -0600

    fix: correct SVG attribute casing in 404 page

--- src/pages/error404/404.tsx
+++ src/pages/error404/404.tsx
@@ -22,8 +22,8 @@ export const Page404 = () => {
                 variant="secondary"
                 onClick={() => navigate(-1)}
               >
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5 rtl:rotate-180">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5 rtl:rotate-180">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
                 </svg>
                 <span>{t('page_404.buttons.back')}</span>
               </Button>
