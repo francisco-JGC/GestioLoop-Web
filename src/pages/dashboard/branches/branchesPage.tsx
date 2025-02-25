cdbf5a4795595244c86896607a8520182343021c
francisco-JGC <jjjchico1@gmail.com>
2025-02-24 19:59:11 -0600

    feat: add branches page and branch item component to dashboard

---
+++ src/pages/dashboard/branches/branchesPage.tsx
@@ -0,0 +1,11 @@
+import { DashboardLayout } from "../../layouts/dashboardLayout"
+
+export const BranchesPage = () => {
+  return (
+    <DashboardLayout>
+      <div>
+        <h1>Branches page wdqwd</h1>
+      </div>
+    </DashboardLayout>
+  )
+}
