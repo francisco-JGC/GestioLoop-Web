d9548e0a6f927ec0c78bfbbc1f5ff74d3dc446d3
francisco-JGC <jjjchico1@gmail.com>
2025-02-24 23:15:11 -0600

    feat: add CreateBranch component to BranchesLists for branch creation functionality

--- src/pages/dashboard/branches/components/branches-lists.tsx
+++ src/pages/dashboard/branches/components/branches-lists.tsx
@@ -1,11 +1,14 @@
 import useBranchesStore from "@/shared/store/branchesStore"
 import { BranchItem } from "./branch-item"
+import { CreateBranch } from "./create-branch"
+
 
 export const BranchesLists = () => {
   const { branches } = useBranchesStore()
 
   return (
     <div className="w-full flex flex-wrap gap-6">
+      <CreateBranch />
       {branches?.map((branch) => (
         <BranchItem branch={branch} key={branch.id} />
       ))}
