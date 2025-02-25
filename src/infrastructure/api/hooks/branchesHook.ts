d46031694519915a64790922c353d897a136e710
francisco-JGC <jjjchico1@gmail.com>
2025-02-24 23:34:20 -0600

    feat: add configureBranch function for branch configuration API

--- src/infrastructure/api/hooks/branchesHook.ts
+++ src/infrastructure/api/hooks/branchesHook.ts
@@ -1,7 +1,15 @@
 import { apiGL } from '../../../core/config/axiosClient'
 import { Branch } from '../types/branch'
+import { HttpResponse } from '../types/httpResponse'
 
 export const getBranches = async (): Promise<Branch[] | null> => {
   const response = await apiGL.get('/branch/branches')
+  return response.data
+}
+
+export const configureBranch = async (
+  branch: Branch
+): Promise<HttpResponse> => {
+  const response = await apiGL.post('/branch/configure-branch', branch)
   return response.data
 }
