7b03aa22e66e18d3d1904cfa69108e9872ccaeca
francisco-JGC <jjjchico1@gmail.com>
2025-02-25 23:49:25 -0600

    feat: integrate translation for UsersManagementPage header

--- src/pages/dashboard/usersManagement/usersManagementPage.tsx
+++ src/pages/dashboard/usersManagement/usersManagementPage.tsx
@@ -1,10 +1,14 @@
+import { HeaderDashboardPage } from "@/pages/layouts/headerDasboardPage"
 import { DashboardLayout } from "../../layouts/dashboardLayout"
+import { useTranslation } from "react-i18next"
 
 export const UsersManagementPage = () => {
+  const { t } = useTranslation()
+
   return (
     <DashboardLayout>
       <div>
-        <h1>User management page</h1>
+        <HeaderDashboardPage title={t('um.title-page')} description={t('um.description-page')} />
       </div>
     </DashboardLayout>
   )
