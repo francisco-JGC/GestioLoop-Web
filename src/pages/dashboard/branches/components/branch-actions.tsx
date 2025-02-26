a3b84f3c09fcf96753a1b9d4e93d1650a217d2fa
francisco-JGC <jjjchico1@gmail.com>
2025-02-25 20:20:19 -0600

    feat: integrate UpdateStatusBranch component into BranchActions

--- src/pages/dashboard/branches/components/branch-actions.tsx
+++ src/pages/dashboard/branches/components/branch-actions.tsx
@@ -4,6 +4,7 @@ import { Branch } from "@/infrastructure/api/types/branch"
 import { Pencil, Sparkles, Trash2, UsersRound } from "lucide-react"
 import { useTranslation } from "react-i18next"
 import { UpdateBranchForm } from "./update-branch-form"
+import { UpdateStatusBranch } from "./update-status-branch"
 
 interface IProps {
   branch: Branch
@@ -52,6 +53,10 @@ export const BranchActions = ({ branch }: IProps) => {
           </DialogContent>
         </Dialog>
 
+        <UpdateStatusBranch
+          branch={branch}
+          className="hover:bg-gray-100 data-[variant=destructive]:text-destructive data-[variant=destructive]:focus:bg-destructive/10 data-[variant=destructive]:focus:text-destructive data-[variant=destructive]:*:[svg]:!text-destructive [&_svg:not([class*='text-'])]:text-muted-foreground relative flex cursor-default items-center gap-2 rounded-sm px-2 py-1.5 text-sm outline-hidden select-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50 data-[inset]:pl-8 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4"
+        />
         <DropdownMenuSeparator />
 
         <DropdownMenuItem className="text-red-500 ">
