43af3e265496f476833cccf6084812f0a9df1a48
francisco-JGC <jjjchico1@gmail.com>
2025-02-26 14:59:28 -0600

    feat: add ItemExternalUser component for displaying external user details in the management interface

---
+++ src/pages/dashboard/usersManagement/components/item-external-user.tsx
@@ -0,0 +1,29 @@
+import { TableCell, TableRow } from "@/components/ui/table"
+import { ExternalUser } from "@/infrastructure/api/types/user"
+import { Actions } from "./actions"
+import { RenderAvatar } from "@/shared/components/renderAvatar"
+
+interface IProps {
+  user: ExternalUser
+}
+
+export const ItemExternalUser = ({ user }: IProps) => {
+  return (
+    <TableRow key={user.id} className="p-10">
+      <TableCell>
+        <RenderAvatar
+          image_url={user.image_url}
+          chart={user.username.charAt(0)}
+        />
+      </TableCell>
+      <TableCell>{user.username}</TableCell>
+      <TableCell>{user.email}</TableCell>
+      <TableCell>{user.phone_number}</TableCell>
+      <TableCell>{user.branch?.name || "N/A"}</TableCell>
+      <TableCell>{user.user_role}</TableCell>
+      <TableCell>
+        <Actions />
+      </TableCell>
+    </TableRow>
+  )
+}
