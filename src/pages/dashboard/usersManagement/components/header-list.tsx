e89044b973426401380049c3c272a149ddacb491
francisco-JGC <jjjchico1@gmail.com>
2025-02-26 14:59:24 -0600

    feat: add HeaderList component for user management interface

---
+++ src/pages/dashboard/usersManagement/components/header-list.tsx
@@ -0,0 +1,27 @@
+import { Input } from "@/components/ui/input"
+import { Button } from "@heroui/button"
+import { Search, Plus } from "lucide-react"
+
+export const HeaderList = () => {
+  return (
+    <div className="flex items-center justify-between">
+      <div>
+        <span className="text-lg font-semibold text-gray-800">
+          All users: <span className="text-gray-500">30</span>
+        </span>
+      </div>
+      <div className="flex gap-4 items-center">
+        <div className="flex items-center">
+          <Search className="absolute ml-4 w-5 h-5 text-gray-400" />
+          <Input className="rounded-full shadow-none px-12" />
+        </div>
+        <div>
+          <Button variant="shadow" className="bg-primary text-white rounded-full">
+            <Plus className="w-5 h-5" />
+            Add User
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
+}
