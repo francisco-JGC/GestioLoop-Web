dbc4a5668fe0ec013eeda487bb420245d6005bc2
francisco-JGC <jjjchico1@gmail.com>
2025-03-04 00:32:04 -0600

    feat: add utility function to format prices in USD

---
+++ src/shared/utils/formatters.ts
@@ -0,0 +1,12 @@
+export function formatPriceToUSD(price: number) {
+  if (isNaN(price)) {
+    return '$0.00'
+  }
+
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(price)
+}
