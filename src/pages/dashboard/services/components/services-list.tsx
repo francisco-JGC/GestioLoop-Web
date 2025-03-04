d82eb19a2224f23281101424878fc5497421da93
francisco-JGC <jjjchico1@gmail.com>
2025-03-03 21:54:29 -0600

    feat: add ServicesList component for displaying services with loading state

---
+++ src/pages/dashboard/services/components/services-list.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import useServicesStore from "@/shared/store/servicesStore"
+import { TFunction } from "i18next"
+import { useEffect } from "react"
+import { ServicesItem } from "./services-item"
+
+interface IProps {
+  t: TFunction
+}
+
+export const ServicesList = ({ t }: IProps) => {
+  const { services, loadServices, isLoading } = useServicesStore()
+
+  useEffect(() => {
+    loadServices()
+  }, [])
+
+  return (
+    <div className="grid grid-cols-2 gap-10">
+      {isLoading ? (
+        <p>Cargando servicios...</p>
+      ) : (
+        services.map((service) => (
+          <ServicesItem key={service.id} service={service} t={t} />
+        ))
+      )}
+    </div>
+  )
+}
