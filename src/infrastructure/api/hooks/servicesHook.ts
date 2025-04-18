7f44ec8901ee6d5f35a157cd6291a3cd1b2cbf68
francisco-JGC <jjjchico1@gmail.com>
2025-04-18 12:38:15 -0600

    feat: add hireServiceById function to servicesHook for hiring services

--- src/infrastructure/api/hooks/servicesHook.ts
+++ src/infrastructure/api/hooks/servicesHook.ts
@@ -1,7 +1,18 @@
 import { apiGL } from '../../../core/config/axiosClient'
+import { HttpResponse } from '../types/httpResponse'
 import { Services } from '../types/services'
 
 export const getServices = async (): Promise<Services[]> => {
   const response = await apiGL.get('/services')
+  return response.data
+}
+
+export const hireServiceById = async (
+  serviceId: string
+): Promise<HttpResponse> => {
+  const response = await apiGL.post('/services/hire', {
+    serviceId,
+  })
+
   return response.data
 }
